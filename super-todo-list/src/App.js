2c8b282a20346bc32fc5216d7e6acd2e3f970797
Alan Badillo <badillo.soft@hotmail.com>
2017-09-26 10:31:27 -0500

    Sesión 2

--- super-todo-list/src/App.js
+++ super-todo-list/src/App.js
@@ -4,15 +4,22 @@ import './App.css';
 
 export default class App extends Component {
   render() {
+    const items = [];
+
+    for (let i = 0; i < 100; i++) {
+      items.push(
+        <li>
+          <input type="checkbox" /> TAREA EJEMPLO { i } 
+        </li>
+      );
+    }
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Super TO-DO List App</h1>
+        <input type="text" placeholder="Escribe una tarea" />
+        <button>agregar</button>
+        <ul>{ items }</ul>
       </div>
     );
   }
