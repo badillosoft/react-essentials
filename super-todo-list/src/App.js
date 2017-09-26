826065f1c2e9d0b7959945d88014e9810f54c902
Alan Badillo <badillo.soft@hotmail.com>
2017-09-26 11:05:16 -0500

    Sesión 2

--- super-todo-list/src/App.js
+++ super-todo-list/src/App.js
@@ -3,23 +3,44 @@ import logo from './logo.svg';
 import './App.css';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      desciption: "",
+      items: [],
+    };
+
+    this.addTask = this.addTask.bind(this);
+    this.onDesciptionUpdate = this.onDesciptionUpdate.bind(this);
+  }
+
+  addTask() {
+    const items = this.state.items;
+
+    items.push(
+      <li>{ this.state.desciption }</li>
+    );
+
+    this.setState({
+      items: items
+    });
+  }
+
+  onDesciptionUpdate(event) {
+    this.setState({
+      desciption: event.target.value
+    });
+  }
+
   render() {
-    const items = [];
-
-    for (let i = 0; i < 100; i++) {
-      items.push(
-        <li>
-          <input type="checkbox" /> TAREA EJEMPLO { i } 
-        </li>
-      );
-    }
-
     return (
       <div className="App">
         <h1>Super TO-DO List App</h1>
-        <input type="text" placeholder="Escribe una tarea" />
-        <button>agregar</button>
-        <ul>{ items }</ul>
+        <input value={this.desciption} onChange={ this.onDesciptionUpdate } type="text" placeholder="Escribe una tarea" />
+        <button onClick={ this.addTask }>agregar</button>
+        <ul>{ this.state.items }</ul>
       </div>
     );
   }
