150067bf4af9e3a8e15a13d7870c615e15d88e43
Alan Badillo <badillo.soft@hotmail.com>
2017-10-03 10:06:37 -0500

    Sesión 7 - v1.0.0 SUPER POST APP

--- super-post/src/views/Post/Post.js
+++ super-post/src/views/Post/Post.js
@@ -3,12 +3,30 @@ import React, { Component } from 'react';
 export default class Post extends Component {
 
     render() {
+        const left_s = Math.floor((new Date() - new Date(this.props.post.at)) / 1000);
+
+        let left = `${left_s} seg`;
+
+        if (left_s >= 60) {
+            const left_m = Math.floor(left_s / 60);
+            left = `${left_m} min`;
+            if (left_m >= 60) {
+                const left_h = Math.floor(left_m / 60);
+                left = `${left_h} horas`;
+
+                if (left_h >= 24) {
+                    const left_d = Math.floor(left_h / 24);
+                    left = `${left_d} dias`;
+                }
+            }
+        }
+
         return (
             <div className="post">
                 <div className="post-avatar">
                     <img src={this.props.post.user.picture} alt="profile" />
                     <a href="#!">{this.props.post.user.displayName}</a>
-                    <span><small>{this.props.post.left}</small></span>
+                    <span><small>{left}</small></span>
                 </div>
                 <div className="post-content">
                     {this.props.post.content}
